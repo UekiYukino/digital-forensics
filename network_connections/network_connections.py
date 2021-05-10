9a61583904b26e76be3b87ab86b845486850f4d0
Khanh Vu <31362665+UekiYukino@users.noreply.github.com>
2021-05-10 09:24:53 -0400

    Add capture invalid API credentials and ability to store unsearched results to a file

--- network_connections/network_connections.py
+++ network_connections/network_connections.py
@@ -1,6 +1,8 @@
 from winreg import *
 import requests
 import json
+import sys
+import os
 
 
 def val2addr(val):
@@ -52,7 +54,14 @@ def query_mac(mac, api_name, api_token):
     query_url = "https://api.wigle.net/api/v2/network/search?netid={}".format(
         mac)
     req = requests.get(query_url, auth=(api_name, api_token))
-    return req.json()
+    if req.status_code == 200:
+        return req.json()
+    elif req.status_code == 401:
+        print("[-] Invalid credentials!")
+        sys.exit(1)
+    else:
+        print("[-] Exit with status code {}".format(req.status_code))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -60,11 +69,11 @@ if __name__ == "__main__":
     networks_dict, count = printNets()
     # Ask user if they want to query the Wigle database
     options = input(
-        "Do you want to query Wigle database for the networks location?(y/n) ")
+        "Do you want to query Wigle database for the networks location? (y/n) ")
 
     if options == "y":
-        # Start counter for network that query successfully on Wigle
-        finished_network = 0
+        # Create a list to store successful queries
+        finished_network = []
 
         # Ask user for their credentials and output location
         api_name = input("Enter your api_name for Wigle: ")
@@ -72,21 +81,41 @@ if __name__ == "__main__":
         out_file = input(
             "Enter your output file name (default: result.json): ") or "result.json"
 
+        json_result = []
         # Iterate through the MAC address dictionary and query MAC address against the Wigle database
         for mac in networks_dict.values():
+            print("[+] Querying for {}...".format(mac))
             w_reply = query_mac(
                 mac, api_name, api_token)
 
             # Append result to the file if the query is successful
             if w_reply["success"] == True:
-                finished_network += 1
-                with open(out_file, "a") as json_output:
-                    json.dump(w_reply, json_output)
+                finished_network.append[mac]
+                print("[+] {} finished".format(mac))
+                json_result.append(w_reply)
+
             # Escape loops and message user if daily query is exceeded
             elif w_reply["message"] == "too many queries today":
                 print("[-] Daily query exceeded!")
                 break
-        print("Finish querying for {}/{} networks! File stored in {}".format(
-            finished_network, count, out_file))
+        print("Finish querying for {}/{} networks!".format(
+            len(finished_network), count))
+
+        # Write the results to a file if the result is not empty
+        if json_result:
+            with open(out_file, "w") as json_output:
+                json.dump(json_result, json_output, indent=3)
+            print("[+] File store in {}\\".format(os.getcwd(), out_file))
+
+        # Process the unsearched results
+        unsearched = []
+        for name, address in networks_dict.items():
+            if address not in finished_network:
+                unsearched.append({name: address})
+        with open("unsearched.json", "w") as left_over:
+            json.dump(unsearched, left_over, indent=3)
+        print(
+            "[*] Unsearched addresses will be store in {}\unsearched.json".format(os.getcwd()))
+
     else:
         print("[+] Exiting!")
